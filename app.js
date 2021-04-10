43c36051c92b69cf6d0133c483127950b43e5b0f
Barzi Jaf <barzi4greatness@gmail.com>
2021-04-10 20:19:43 +0200

    Improved it

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-let percent_textbox = document.querySelector('.percent-tb');
-let value_textbox = document.querySelector('.value-tb');
-let label_result = document.querySelector('label[for="result"]')
+let percentInput = document.querySelector('input#percent-number');
+let valueInput = document.querySelector('input#whole-number');
+let labelResult = document.querySelector('label#result')
 let calculate_button = document.querySelector('input[type="button"]');
 
-function DoIt() {
-
-  function remove_non_numeric() {
-    percent_textbox.value = percent_textbox.value.replace(/[^\d.-]/g, '');
-    value_textbox.value = value_textbox.value.replace(/[^\d.-]/g, '');
-  }
-  percent_textbox.addEventListener('change', remove_non_numeric);
-  value_textbox.addEventListener('change', remove_non_numeric);
-
-  function calculation_process () {
-    var calculation;
-    if(percent_textbox.value > 100)
-      {percent_textbox.value = 100;}
-
-      calculation = percent_textbox.value * value_textbox.value;
-      calculation = calculation / 100;
-      label_result.innerHTML = "Result: " + calculation;
-    }
+calculate_button.addEventListener('click', calculate);
 
 
-  calculate_button.addEventListener('click', calculation_process);
+function calculate() {
+  let result;
+  if(percentInput.value > 100) 
+    percentInput.value = 100;
+
+    result = percentInput.value * valueInput.value;
+    result = result / 100;
+    labelResult.innerHTML = `( ${percentInput.value}% of ${valueInput.value} is <em>${result} )</em> <br> <br>
+    <em> meaning if a price of a jacket is $${valueInput.value}, 
+    but you get a discount of ${percentInput.value}%, you <strong>save $${result}</strong> that means you get the <strong>jacket for $${parseFloat(valueInput.value - result).toFixed(2)}</strong></em> ;D`;
 }
 
-DoIt();
 
